0a6f44a200d8a7752f9e2f6a6b1d01f51ea8d635
jkruk-git <jkruk@ucsd.edu>
2024-12-05 13:56:29 -0800

    Update musicQuiz.js
    needed the new js with it

--- musicQuiz.js
+++ musicQuiz.js
@@ -5,32 +5,17 @@ function calculateResult() {
     // Initialize a counter object to tally up each music type
     const results = {
         pop: 0,
-<<<<<<< Updated upstream
-=======
-        rock: 0, // Updated for consistency
->>>>>>> Stashed changes
+        rock: 0,
         classical: 0,
         rap: 0,
         house: 0
     };
 
-<<<<<<< Updated upstream
-    // Get the answers for each question
-    const q1 = form.elements['q1'].value;
-    const q2 = form.elements['q2'].value;
-    const q3 = form.elements['q3'].value;
-    const q4 = form.elements['q4'].value;
-    // Count the selected options
-    results[q1]++;
-    results[q2]++;
-    results[q3]++;
-    results[q4]++;
-=======
     // Validate that all questions have been answered
     let allAnswered = true;
 
     // Collect answers
-    const questions = ['q1', 'q2', 'q3', 'q4'];
+    const questions = ['q1', 'q2', 'q3'];
     questions.forEach(question => {
         const answer = form.elements[question]?.value;
 
@@ -46,7 +31,6 @@ function calculateResult() {
         document.getElementById("result").innerText = "Please answer all the questions!";
         return;
     }
->>>>>>> Stashed changes
 
     // Determine the highest score
     let finalResult = "";
